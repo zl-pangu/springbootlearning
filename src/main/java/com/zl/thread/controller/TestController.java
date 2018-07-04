ae025c1137abc89b168d08c254591705e2d41786
周良 <liangzhou@sf-express.com>
2018-07-04 13:44:58 +0800

    update:修改properties类型的配置文件为yml，偏于分层显示，修改testcontrollerredis和mapper的查询

--- src/main/java/com/zl/thread/controller/TestController.java
+++ src/main/java/com/zl/thread/controller/TestController.java
@@ -1,11 +1,18 @@
 package com.zl.thread.controller;
 
-import com.zl.thread.dto.CustTableMatchDto;
-import com.zl.thread.service.CustTableMatchService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zl.common.redis.RedisService;
+import com.zl.common.utils.R;
+import com.zl.thread.dao.CustUploadingFilesDao;
+import com.zl.thread.domain.CustUploadingFilesDO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
 
 @Controller
 @RequestMapping("zl")
@@ -13,20 +20,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @Autowired
-    private CustTableMatchService custTableMatchService;
+    private CustUploadingFilesDao dao;
 
-    @RequestMapping("/test")
+    @Resource
+    RedisService redisService;
+
+    @RequestMapping("/do")
     public String test(){
-       log.info("进入一个测试页面。。。。");
+        log.info("进入一个测试页面。。。。");
         return "test";
     }
 
-    @RequestMapping("test1")
-    public void test1() throws Exception{
-        CustTableMatchDto dto=new CustTableMatchDto();
-        dto.setFlag("123456");
-        dto.setUserNo("01376493");
-        custTableMatchService.parseExcles(dto);
+    @RequestMapping("/queryFile")
+    @ResponseBody
+    public String test1(){
+        log.info("queryFile。。。。。");
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            CustUploadingFilesDO files = dao.get(22405);
+            String str = mapper.writeValueAsString(files);
+            return str;
+        } catch (Exception e) {
+            log.error("查询失败：{}",e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("/redisTest")
+    @ResponseBody
+    public R redisTest(){
+        log.info("redis test....");
+        long l = System.currentTimeMillis();
+        String msg = null;
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            CustUploadingFilesDO files = dao.get(22405);
+            msg = redisService.setObject(String.valueOf(l), files, 300);
+            CustUploadingFilesDO obj = (CustUploadingFilesDO)redisService.getObject(String.valueOf(l));
+            log.info("save objValue:{}",mapper.writeValueAsString(obj));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return R.ok(msg);
     }
 
 }
